1ef1105fbc9ce13e8803162cbb6cfc1e036b282c
Natalya Sorokina <natali83911@gmail.com>
2025-04-29 00:15:38 +0300

    modified file

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ from src.services import analyze_cashback_categories
 from src.views import main_info
 
 if __name__ == "__main__":
-    # print(main_info("2021-12-15 15:30:00"))
-    #
-    # print(analyze_cashback_categories(PATH_TO_EXCEL, 2018, 3))
+    print(main_info("2021-12-15 15:30:00"))
+
+    print(analyze_cashback_categories(PATH_TO_EXCEL, 2018, 3))
 
     df = get_data_from_file(PATH_TO_EXCEL)
     result = expenses_by_category(df, "Аптеки", "2019-04-01")
